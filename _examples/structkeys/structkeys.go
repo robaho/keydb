e1f9a6483dc90ae985a421cc8a36ea9265607f39
robaho <rengels@ix.netcom.com>
2018-09-01 12:59:30 -0500

    examples of using structures as keys

---
+++ _examples/structkeys/structkeys.go
@@ -0,0 +1,103 @@
+package structkeys
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/robaho/keydb"
+	"strings"
+	"time"
+)
+
+// demonstrate the use of structures as keys
+
+type MyKey struct {
+	Symbol string
+	At     time.Time
+}
+
+func (mk MyKey) MarshalBinary() ([]byte, error) {
+	// A simple encoding: plain text.
+	var b bytes.Buffer
+	fmt.Fprintln(&b, mk.Symbol, mk.At)
+	return b.Bytes(), nil
+}
+
+// UnmarshalBinary modifies the receiver so it must take a pointer receiver.
+func (mk *MyKey) UnmarshalBinary(data []byte) error {
+	// A simple encoding: plain text.
+	b := bytes.NewBuffer(data)
+	_, err := fmt.Fscanln(b, &mk.Symbol, &mk.At)
+	return err
+}
+
+func (*MyKey) Less(a []byte, b []byte) bool {
+	_a := MyKey{}
+	_b := MyKey{}
+
+	_a.UnmarshalBinary(a)
+	_b.UnmarshalBinary(b)
+
+	c := strings.Compare(_a.Symbol, _b.Symbol)
+	if c == 0 {
+		return _a.At.Before(_b.At)
+	}
+	return c < 0
+}
+
+func main() {
+
+	path := "test/structkeys"
+
+	keydb.Remove(path)
+	db, err := keydb.Open(path, []keydb.Table{{"main", &MyKey{}}}, true)
+	if err != nil {
+		panic(err)
+	}
+
+	tx, err := db.BeginTX("main")
+	if err != nil {
+		panic(err)
+	}
+
+	a := MyKey{"ibm", time.Now()}
+	b := MyKey{"aapl", time.Now()}
+
+	_a, _ := a.MarshalBinary()
+	_b, _ := b.MarshalBinary()
+
+	tx.Put(_a, []byte("some value for a"))
+	tx.Put(_b, []byte("some value for b"))
+
+	tx.Commit()
+
+	tx, err = db.BeginTX("main")
+	if err != nil {
+		panic(err)
+	}
+	value, err := tx.Get(_a)
+	if string(value) != "some value for a" {
+		panic("wrong a value")
+	}
+
+	itr, err := tx.Lookup(nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	key, value, err := itr.Next()
+
+	mykey := MyKey{}
+	mykey.UnmarshalBinary(key)
+
+	if mykey.Symbol != "aapl" {
+		panic("wrong key")
+	}
+
+	key, value, err = itr.Next()
+	mykey.UnmarshalBinary(key)
+	if mykey.Symbol != "ibm" {
+		panic("wrong key")
+	}
+
+	tx.Commit()
+	db.Close()
+}
---
+++ _examples/structkeys2/structkeys2.go
@@ -0,0 +1,73 @@
+package structkeys
+
+import (
+	"github.com/robaho/keydb"
+	. "github.com/robaho/keydb/_examples/structkeys"
+	"time"
+)
+
+type MyKeyTx struct {
+	tx *keydb.Transaction
+}
+
+func (mktx *MyKeyTx) Get(key MyKey) (string, error) {
+	_key, err := key.MarshalBinary()
+	if err != nil {
+		return "", err
+	}
+
+	val, err := mktx.tx.Get(_key)
+	return string(val), err
+}
+func (mktx *MyKeyTx) Put(key MyKey, value string) error {
+	_key, err := key.MarshalBinary()
+	if err != nil {
+		return err
+	}
+	return mktx.tx.Put(_key, []byte(value))
+}
+
+func main() {
+
+	path := "test/structkeys"
+
+	keydb.Remove(path)
+	db, err := keydb.Open(path, []keydb.Table{{"main", &MyKey{}}}, true)
+	if err != nil {
+		panic(err)
+	}
+
+	tx, err := db.BeginTX("main")
+	if err != nil {
+		panic(err)
+	}
+
+	mktx := MyKeyTx{tx}
+
+	a := MyKey{"ibm", time.Now()}
+	b := MyKey{"aapl", time.Now()}
+
+	mktx.Put(a, "some value for a")
+	mktx.Put(b, "some value for b")
+
+	tx.Commit()
+
+	tx, err = db.BeginTX("main")
+	if err != nil {
+		panic(err)
+	}
+
+	mktx = MyKeyTx{tx}
+
+	value, err := mktx.Get(a)
+	if value != "some value for a" {
+		panic("wrong a value")
+	}
+	value, err = mktx.Get(b)
+	if value != "some value for b" {
+		panic("wrong a value")
+	}
+
+	tx.Commit()
+	db.Close()
+}
