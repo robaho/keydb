ecf3a0c7ee4510a0245177918a94ed32abe43e82
robaho <rengels@ix.netcom.com>
2018-09-07 16:38:38 -0500

    time partial scan

--- _examples/performance/performance.go
+++ _examples/performance/performance.go
@@ -27,7 +27,7 @@ func main() {
 		panic(err)
 	}
 	for i := 0; i < 1000000; i++ {
-		tx.Put([]byte(fmt.Sprint("mykey", i)), []byte(fmt.Sprint("myvalue", i)))
+		tx.Put([]byte(fmt.Sprintf("mykey%7d", i)), []byte(fmt.Sprint("myvalue", i)))
 		if i%10000 == 0 {
 			tx.Commit()
 			tx, err = db.BeginTX("main")
@@ -48,7 +48,7 @@ func main() {
 
 	db, err = keydb.Open("test/mydb", tables, false)
 	if err != nil {
-		log.Fatal("unable to create database", err)
+		log.Fatal("unable to open database", err)
 	}
 	start = time.Now()
 	tx, err = db.BeginTX("main")
@@ -70,12 +70,25 @@ func main() {
 	fmt.Println("scan time ", (time.Now().Sub(start)).Nanoseconds()/1000000.0, "ms")
 
 	start = time.Now()
+	itr, err = tx.Lookup([]byte("mykey 300000"), []byte("mykey 799999"))
+	count = 0
+	for {
+		_, _, err = itr.Next()
+		if err != nil {
+			break
+		}
+		count++
+	}
+	if count != 500000 {
+		log.Fatal("incorrect count != 500000, count is ", count)
+	}
+	fmt.Println("scan 50% records ", (time.Now().Sub(start)).Nanoseconds()/1000000.0, "ms")
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	for i := 0; i < 100000; i++ {
 		index := r.Intn(1000000)
-		_, err := tx.Get([]byte(fmt.Sprint("mykey", index)))
+		_, err := tx.Get([]byte(fmt.Sprintf("mykey%7d", index)))
 		if err != nil {
 			panic(err)
 		}
